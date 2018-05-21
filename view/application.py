f85f7e687ca16359ad4d51ccfd987d4e3100c478
marcosfelipp <mfelipe11@yahoo.com.br>
2018-05-20 22:15:24 -0300

    Add project structure

---
+++ view/application.py
@@ -0,0 +1,19 @@
+from connection import autentication
+
+class Application:
+    def __init__(self):
+        pass
+
+    def start_connection(self):
+        pass
+
+
+    def print_menu_login(self):
+        '''
+        Print Menu containing login
+        :return: None
+        '''
+        pass
+
+if __name__ == "__main__":
+    Application()
